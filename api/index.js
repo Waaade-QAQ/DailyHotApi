4daca48ae1246529ee5dacc3463f23b613f13efd
weichong <huangwc@iregene.com>
2026-09-08 11:39:31 +0800

    fix(vercel): use @hono/node-server getRequestListener for Node serverless handler

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
-import { handle } from "hono/vercel";
+import { getRequestListener } from "@hono/node-server";
 import app from "../dist/src/app.js";
 
 export const config = {
   runtime: "nodejs",
 };
 
-export default handle(app);
+export default getRequestListener(app.fetch);
+
